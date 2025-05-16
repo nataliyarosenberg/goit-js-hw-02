805ade90b3c2d03b682352f9a120eab36e016bf5
nataliyarosenberg <nshcherbak@yahoo.com>
2025-05-15 20:01:19 -0700

    tasks completed

---
+++ js/task-3.js
@@ -0,0 +1,27 @@
+function checkForSpam(message) {
+  const lowerCase = message.toLowerCase();
+  if (lowerCase.includes('spam') || lowerCase.includes('sale')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+function checkForSpam(message) {
+  const lower = message.toLowerCase();
+  return lower.includes('spam') || lower.includes('sale');
+}
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
